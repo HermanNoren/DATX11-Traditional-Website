90aabdba39395f0e9a476b5d55e6537da11a2083
hannistuis <hannah.tuu.hannah@gmail.com>
2025-03-03 15:21:46 +0100

    homepage done
    buy button does not work yet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Navbar from "./components/navbar/navbar";
+import Homepage from "./pages/homepage/homepage";
 
 export default function Home() {
   return (
     <div className="main">
       <Navbar></Navbar>
+      <Homepage></Homepage>
     </div>
   );
 }
