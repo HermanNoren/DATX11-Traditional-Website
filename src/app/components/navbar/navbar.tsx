320543722a6fea39e6da143231ad59ca8a32f6ab
hannistuis <hannah.tuu.hannah@gmail.com>
2025-03-03 16:43:06 +0100

    header implemented in aboutpage

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -5,12 +5,12 @@ import { Boxes } from "lucide-react";
 function Navbar() {
   return (
     <nav className={"navbar"}>
-      <Link href="/" className="logo">
+      <Link href="#" className="logo">
         <Boxes strokeWidth={1} className="logo-icon" />
       </Link>
       <div className={"navLinks"}>
         <Link href="#">Products</Link>
-        <Link href="#">About Us</Link>
+        <Link href="/aboutpage">About Us</Link>
         <Link href="#">Contacts</Link>
       </div>
     </nav>
---
+++ src/app/pages/aboutpage/aboutpage.tsx
@@ -0,0 +1,11 @@
+import "./aboutpage.css";
+
+function Aboutpage() {
+  return (
+    <div className="aboutpage">
+      <div className="header"></div>
+    </div>
+  );
+}
+
+export default Aboutpage;
