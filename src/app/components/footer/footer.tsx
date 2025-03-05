6236a75f34ef7371efe4f05e2c9ea0e0a7dffb7a
hildalange <hilda.lange@gmail.com>
2025-03-05 19:38:13 +0100

    footer

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -4,25 +4,42 @@ import { Facebook, Instagram, Linkedin, Boxes } from "lucide-react";
 
 function Footer() {
   return (
-    <nav className={"footer"}>
+    <footer className="footer-container">
+      <div className="footer-info">
+        <div className="info-column">
+          <p>View products</p>
+          <button className="footer-button">Products</button>
+        </div>
+        <div className="info-column">
+          <p>About deCube</p>
+          <button className="footer-button">About Us</button>
+        </div>
+        <div className="info-column">
+          <p>Contact us</p>
+          <button className="footer-button">Contact</button>
+        </div>
+      </div>
+      
+      <nav className="footer">
         <text className="copyright"> © deCube</text>
         <div className="logo">
-            <Link href="/" className="logo">
-                <Boxes strokeWidth={1} className="logo-icon" />
-            </Link>
+          <Link href="/" className="logo">
+            <Boxes strokeWidth={1} className="logo-icon" />
+          </Link>
         </div>
         <div className="footerLinks">
-            <Link href="#" className="facebook">
-                <Facebook strokeWidth={1} className="logo-facebook" />
-            </Link>
-            <Link href="#" className="instagram">
-                <Instagram strokeWidth={1} className="logo-instagram" />
-            </Link>
-            <Link href="#" className="linkedin">
-                <Linkedin strokeWidth={1} className="logo-linkedIn" />
-            </Link>
+          <Link href="#" className="facebook">
+            <Facebook strokeWidth={1} className="logo-facebook" />
+          </Link>
+          <Link href="#" className="instagram">
+            <Instagram strokeWidth={1} className="logo-instagram" />
+          </Link>
+          <Link href="#" className="linkedin">
+            <Linkedin strokeWidth={1} className="logo-linkedIn" />
+          </Link>
         </div>
-    </nav>
+      </nav>
+    </footer>
   );
 }
 
