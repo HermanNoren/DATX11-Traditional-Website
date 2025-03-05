a131db90de9d05306c7da7667072e60d53e764b1
Hanna <hanna.carling@hotmail.com>
2025-03-05 21:31:04 +0100

    Added text and images from figma to contactpage

---
+++ src/app/pages/contactpage/contactpage.tsx
@@ -0,0 +1,39 @@
+import "./contactpage.css";
+
+function Contactpage() {
+    return (
+        <div className="contactpage">
+            <div className="header"></div>
+            <h1 className="contact-title">- Contact -</h1> 
+            <div className="contactContainer"></div>
+
+            <div className="sections">
+            <img
+            src="/TransparentCube.png"
+            className="transparent-image"
+          />
+          <div className="deCube">
+            <p className="text">
+               <div className="square">
+               <h2 className="title">- deCube HQ -</h2>
+               <h3 className="subtitle">contact & location</h3>
+                    <span className="bold">Phone:</span><br></br> +46 123 456 789 <br></br>
+                    <span className="bold">Email:</span><br></br> deCube@deCube.com <br></br>
+                    <span className="bold">Adress:</span><br></br> decube street<br></br> Gothenburg Sweden<br></br>
+                    <span className="bold">Socials:</span>
+                </div>
+            </p>
+          </div>
+          
+          <img
+            src="/ContactHQ.png"
+            className="HQimage"
+          />
+        </div>
+
+           
+           </div>
+            );
+}
+
+            export default Contactpage;
