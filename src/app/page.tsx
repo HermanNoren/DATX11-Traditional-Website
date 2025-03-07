d66ab515a5db105d30d16273e58bb1eeac54cc2f
hannistuis <hannah.tuu.hannah@gmail.com>
2025-03-07 10:07:36 +0100

    homepage done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import Navbar from "./components/navbar/navbar";
-import Homepage from "./pages/homepage/homepage";
+import Homepage from "./homepage/page";
 import Footer from "./components/footer/footer";
-import Aboutpage from "./pages/aboutpage/aboutpage";
+import Aboutpage from "./aboutpage/page";
 
 export default function Home() {
   return (
