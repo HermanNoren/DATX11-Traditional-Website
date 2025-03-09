f7487f83cb38e7362b1611243bf8a3233a68a119
hannistuis <hannah.tuu.hannah@gmail.com>
2025-03-09 13:35:57 +0100

    bugfix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,7 @@ import Footer from "./components/footer/footer";
 export default function Home() {
   return (
     <div className="main">
-      <Navbar></Navbar>
       <Homepage></Homepage>
-      <Footer></Footer>
     </div>
   );
 }
